902f648c8d61cb886f6fd59fda735b0bac904eee
aaron-plahn <aplahn85@gmail.com>
2020-11-14 11:27:44 -0800

    feat(game) Scaffold app directory structure
    Organize components based on a page vs. widget classification. A page is a component
    that has a registered route, whereas a widget does not.

--- src/app/app.module.tns.ts
+++ src/app/app.module.tns.ts
@@ -3,7 +3,10 @@ import { NativeScriptModule } from '@nativescript/angular';
 
 import { AppRoutingModule } from '@src/app/app-routing.module';
 import { AppComponent } from '@src/app/app.component';
-import { HomeComponent } from '@src/app/home/home.component';
+import { HomeComponent } from '@src/app/components/pages/home/home.component';
+import { MenuComponent } from './components/pages/menu/menu.component';
+import { GameComponent } from './components/pages/game/game.component';
+import { CardComponent } from './components/widgets/card/card.component';
 
 
 // Uncomment and add to NgModule imports if you need to use two-way binding and/or HTTP wrapper
@@ -13,6 +16,9 @@ import { HomeComponent } from '@src/app/home/home.component';
   declarations: [
     AppComponent,
     HomeComponent,
+    MenuComponent,
+    GameComponent,
+    CardComponent,
   ],
   imports: [
     NativeScriptModule,
